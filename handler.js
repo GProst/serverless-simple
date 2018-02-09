0e11ad69e64994bae6746fbb5fdee42ce37db5d6
GProst <gprostmail@gmail.com>
2018-02-09 10:42:14 +0300

    Update handler to fetch data from DynamoDB

--- handler.js
+++ handler.js
@@ -1,14 +1,35 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      method: event.httpMethod,
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
 
-  callback(null, response);
-};
+module.exports.hello = (event, context, callback) => {
+  const TableName = 'booksTable'
+
+  switch (event.httpMethod) {
+    case 'GET': {
+      dynamodb.scan({TableName}).promise()
+        .then(result => {
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+              result,
+              input: event
+            })
+          })
+        })
+        .catch(callback)
+      break
+    }
+    case 'POST': {
+
+      break
+    }
+    default: {
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({message: `This API doesn't support HTTP Method '${event.httpMethod}'`})
+      })
+    }
+  }
+
+}
